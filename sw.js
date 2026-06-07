4ae8261041a35a0cf1af694f1461508d1a382e93
Milton Ruiz <miltonruiz15@hotmail.com>
2026-06-07 16:31:14 -0500

    Update service worker cache version to v27

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Versión Autolimpiante
-const CACHE_NAME = 'punku-open-v26'; // <--- CAMBIA ESTE NÚMERO
+const CACHE_NAME = 'punku-open-v27'; // <--- CAMBIA ESTE NÚMERO
 
 self.addEventListener('install', event => {
   self.skipWaiting(); // No pidas permiso, instálate ya
