cbddc2db4bf65cf7adc27b37feb1ef4b07c8ca01
Milton Ruiz <miltonruiz15@hotmail.com>
2026-06-13 21:21:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Versión Autolimpiante
-const CACHE_NAME = 'punku-open-v30'; // <--- CAMBIA ESTE NÚMERO
+const CACHE_NAME = 'punku-open-v31'; // <--- CAMBIA ESTE NÚMERO
 
 self.addEventListener('install', event => {
   self.skipWaiting(); // No pidas permiso, instálate ya
