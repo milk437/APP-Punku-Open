86e4df3b07675fff39cb69e85e4824309f3b2743
Milton Ruiz <miltonruiz15@hotmail.com>
2026-06-28 18:26:08 -0500

    Update service worker cache version to v38

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Versión Autolimpiante
-const CACHE_NAME = 'punku-open-v37'; // <--- CAMBIA ESTE NÚMERO
+const CACHE_NAME = 'punku-open-v38'; // <--- CAMBIA ESTE NÚMERO
 
 self.addEventListener('install', event => {
   self.skipWaiting(); // No pidas permiso, instálate ya
