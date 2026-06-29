5916c4c2838d669a1c74afab91d2de65805530d9
Milton Ruiz <miltonruiz15@hotmail.com>
2026-06-28 21:03:20 -0500

    Refactor sw.js to use BASE_PATH for resource caching
    Updated service worker to include BASE_PATH for caching resources.

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-// sw.js - Versión Autolimpiante v40
+// sw.js - Versión Autolimpiante v41
 const CACHE_NAME = 'punku-open-v41';
+const BASE_PATH = '/APP-Punku-Open';
 
 // Instalación inmediata con cacheo de recursos clave
 self.addEventListener('install', event => {
@@ -7,11 +8,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192.png',
-        '/icon-512.png'
+        BASE_PATH + '/',
+        BASE_PATH + '/index.html',
+        BASE_PATH + '/manifest.json',
+        BASE_PATH + '/icon-192.png',
+        BASE_PATH + '/icon-512.png'
       ]);
     })
   );
