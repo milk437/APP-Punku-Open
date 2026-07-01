ebcfb01715ec4c950b4936f44c99342f2c4995f9
Milton Ruiz <miltonruiz15@hotmail.com>
2026-06-30 19:25:46 -0500

    Update service worker to version v43

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Versión Autolimpiante v41
-const CACHE_NAME = 'punku-open-v42';
+const CACHE_NAME = 'punku-open-v43';
 
 // Instalación inmediata con cacheo de recursos clave
 self.addEventListener('install', event => {
