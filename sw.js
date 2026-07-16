e5866a8c0a4bbe083430104823465672c4c0aafa
Milton Ruiz <miltonruiz15@hotmail.com>
2026-07-16 12:16:15 -0500

    Update service worker to version 64 and cache name v65

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js - Versión Autolimpiante v63
-const CACHE_NAME = 'punku-open-v64';
+// sw.js - Versión Autolimpiante v64
+const CACHE_NAME = 'punku-open-v65';
 
 // Instalación inmediata con cacheo de recursos clave
 self.addEventListener('install', event => {
